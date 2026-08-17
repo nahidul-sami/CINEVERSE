444a27c9029a654dac65ae4b82a3d82bb7c97bba
nahidul-sami <2405031@ugrad.cse.buet.ac.bd>
2026-08-17 13:06:42 +0600

    Merge branch 'main' of https://github.com/nahidul-sami/CINEVERS

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,7 +6,8 @@ const { verifyToken } = require("../middleware/authMiddleware");
 // পাবলিক রাউট (Login/Register)
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/profile", verifyToken, getProfile);
+router.put("/profile", verifyToken, updateProfile);
 // প্রটেক্টেড রাউট (Profile)
 router.get("/profile", verifyToken, getUserProfile);
 router.put("/profile", verifyToken, updateUserProfile);
