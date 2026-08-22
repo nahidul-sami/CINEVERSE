083a6bd4c0a6ee130494e109ed97f469ccdfbe33
Mahidul Omi <2405055@ugrad.cse.buet.ac.bd>
2026-08-22 11:19:48 +0600

    Update server.js with latest changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./src/routes/authRoutes");
+
 
 const app = express();
 
@@ -12,6 +12,11 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/persons", personRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/watch-history", watchHistoryRoutes);
+app.use("/api/watchlist", watchlistRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
